0f0e5b3c504c1ab77f018203c96f04f060a8ead3
eric <aohanhongzhi@qq.com>
2026-01-14 16:40:28 +0800

    nv12

--- quick-start.sh
+++ quick-start.sh
@@ -47,7 +47,7 @@ exec go run bytetrack.go \
   -t v8pose \
   -l ../data/yolov8_pose_labels_list.txt \
   -c "640x480@10" \
-  -codec yuyv \
+  -codec nv12 \
   -p rk3566 \
   -x person \
   -a :8081
